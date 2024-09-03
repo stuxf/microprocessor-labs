c2fdead5f7e4c1c466d8f4f8d60bb6ec8e90dbbc
user <70670632+stuxf@users.noreply.github.com>
2024-09-02 22:05:02 -0700

    🐛 fix some more led oopsies

--- lab-1-setup-and-testing/fpga/src/seven_segment.sv
+++ lab-1-setup-and-testing/fpga/src/seven_segment.sv
@@ -30,13 +30,13 @@ module seven_segment(
             4'h5:       segments = 7'b0100100;
             4'h6:       segments = 7'b0100000;
             4'h7:       segments = 7'b0001111;
-            4'h8:       segments = 7'b0001111;
+            4'h8:       segments = 7'b1111111;
             4'h9:       segments = 7'b0001100;
-            4'ha:       segments = 7'b0001001;
-            4'hb:       segments = 7'b1100001;
+            4'ha:       segments = 7'b0001000;
+            4'hb:       segments = 7'b1100000;
             4'hc:       segments = 7'b0110001;
             4'hd:       segments = 7'b1000010;
-            4'he:       segments = 7'b0000110;
+            4'he:       segments = 7'b0110000;
             4'hf:       segments = 7'b0111000;
             default:    segments = 7'b1111111;
         endcase
