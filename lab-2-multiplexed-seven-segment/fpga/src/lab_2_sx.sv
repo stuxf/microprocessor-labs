6105f8cabfe058bade26afc06e577eaed8dbfab9
user <70670632+stuxf@users.noreply.github.com>
2024-09-08 21:11:04 -0700

    🎉 Add top level file for Lab 2

---
+++ lab-2-multiplexed-seven-segment/fpga/src/lab_2_sx.sv
@@ -0,0 +1,29 @@
+/* 
+ * Stephen Xu
+ * September 8th, 2024
+ * stxu@g.hmc.edu
+ * This is the top level module for the second lab
+ * Its purpose is to allow for us to control two seven segment displays
+ * We control them using two DIP switches
+ * It also includes logic to display the sum of the switches
+ * We use a time based MUX in order to alternate displays
+ */
+
+module lab_2_sx(
+    // Reset Switch
+    input logic reset,
+    // Four DIP switches on the board
+    input logic [3:0] s1,
+    // Additionally wired four DIP switches
+    input logic [3:0] s2,
+    // Indicates whether or not one of the displays is on
+    output logic on1,
+    // Indicates whether or not the other display is on
+    output logic on2,
+    // What the segments are
+    output logic [6:0] seg,
+    // What the LED represents
+    output logic [4:0] led
+);
+ 
+endmodule
