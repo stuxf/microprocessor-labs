e671f7dc65a8a8c59a9b7699c3b072451428ed57
user <70670632+stuxf@users.noreply.github.com>
2024-09-17 14:56:03 -0700

    add fractional clock divider tests and modify slightly

--- lab-2-multiplexed-seven-segment/fpga/src/fractional_clk_div.sv
+++ lab-2-multiplexed-seven-segment/fpga/src/fractional_clk_div.sv
@@ -1,30 +1,22 @@
-/* 
- * Stephen Xu
- * September 8th, 2024
- * stxu@g.hmc.edu
- * This is a fractional clock divider
- * Is meant to toggle a signal on and off
- * It does so after every n clocks
- */
-
 module fractional_clk_div(
     input logic clk,
     input logic reset,
     output logic out_clk
 );
 
-    integer counter;
+    logic [15:0] counter;  
 
     always_ff @(posedge clk) begin
         if (reset == 0) begin
-            counter <= 1;
+            counter <= '0;
             out_clk <= 0;
-        end
-        if (counter >= 25000) begin
-            out_clk <= ~out_clk;
-            counter <= 1;
         end else begin
-            counter <= counter + 1;
+            if (counter >= 16'd24999) begin 
+                out_clk <= ~out_clk;
+                counter <= '0;
+            end else begin
+                counter <= counter + 16'd1;
+            end
         end
     end
     
---
+++ lab-2-multiplexed-seven-segment/fpga/src/fractional_clk_div_tb.sv
@@ -0,0 +1,46 @@
+`timescale 1ns/1ns
+`default_nettype none
+
+module fractional_clk_div_tb();
+
+    // Declare signals
+    logic clk;
+    logic reset;
+    logic out_clk;
+
+    // Instantiate the Device Under Test (DUT)
+    fractional_clk_div dut (
+        .clk(clk),
+        .reset(reset),
+        .out_clk(out_clk)
+    );
+
+
+    // Generate clock signal with a period of 10 timesteps.
+    always begin
+        clk = 1; #5;
+        clk = 0; #5;
+    end
+
+    // Testbench stimulus
+    initial begin
+        // Initialize signals
+        reset = 0;  // Active low reset
+        
+        // Apply reset
+        #20 reset = 1;  // Release reset
+
+        // Wait for multiple output clock cycles
+        repeat(250000) @(posedge clk);
+
+        // End simulation
+        $finish;
+    end
+
+    // Monitor output and internal signals
+    initial begin
+        $monitor("Time=%0t ns: reset=%b, out_clk=%b, counter=%d", 
+                 $time, reset, out_clk, dut.counter);
+    end
+
+endmodule
