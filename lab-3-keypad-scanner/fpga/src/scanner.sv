664d820a5abbb0e9277901c0e7fd9ef456750616
user <70670632+stuxf@users.noreply.github.com>
2024-09-22 14:36:14 -0700

    ♻️ Remove debouncer

--- lab-3-keypad-scanner/fpga/src/scanner.sv
+++ lab-3-keypad-scanner/fpga/src/scanner.sv
@@ -18,6 +18,7 @@ module scanner(
 );
 
     // Use synchronizer for metastability
+    // Adds two clock delay
     logic [3:0] stable_rows;
     sync synchronizer (
         clk,
@@ -25,23 +26,18 @@ module scanner(
         stable_rows
     );
 
-    typedef enum logic [3:0] {
+    typedef enum logic [2:0] {
         col0,
-        col0_check,
+        col0_hold,
         col1,
-        col1_check,
+        col1_hold,
         col2,
-        col2_check,
+        col2_hold,
         col3,
-        col3_check,
-        debounce,
-        pressed
+        col3_hold
     } statetype;
 
     statetype state, nextstate;
-
-    // Internal Logic
-    logic [5:0] counter;
 
     // State register
     always_ff @(posedge clk)
@@ -52,57 +48,40 @@ module scanner(
     always_comb begin
 
         case (state)
-            col0:       nextstate = col0_check;
-            col0_check: if ($onehot(stable_rows)) begin
-                            nextstate = debounce;
+            col0: if ($onehot(stable_rows)) begin
+                            nextstate = col2_hold;
                         end
                         else nextstate = col1;
-            col1:       nextstate = col1_check;
-            col1_check: if ($onehot(stable_rows)) begin
-                            nextstate = debounce;
+            col1: if ($onehot(stable_rows)) begin
+                            nextstate = col3_hold;
                         end
                         else nextstate = col2;
-            col2: nextstate = col2_check;
-            col2_check: if ($onehot(stable_rows)) begin
-                            nextstate = debounce;
+            col2: if ($onehot(stable_rows)) begin
+                            nextstate = col0_hold;
                         end
                         else nextstate = col3;
-            col3:       nextstate = col3_check;
-            col3_check: if ($onehot(stable_rows)) begin
-                            nextstate = debounce;
+            col3: if ($onehot(stable_rows)) begin
+                            nextstate = col1_hold;
                         end
                         else nextstate = col0;
-            debounce:   if (counter >= 50) begin
-                            nextstate = pressed;
-                        end
-                        else if (!stable_rows[pressed_row]) begin
-                            nextstate = col0;
-                        end 
-                        else nextstate = debounce;
-            pressed:    if (stable_rows == 0) begin
-                            nextstate = col0;
-                        end
-                        else nextstate = pressed;
+            col0_hold: if (stable_rows[pressed_row]) begin
+                            nextstate = col0_hold;
+                       end
+                       else nextstate = col0;
+            col1_hold: if (stable_rows[pressed_row]) begin
+                            nextstate = col1_hold;
+                       end
+                       else nextstate = col1;
+            col2_hold: if (stable_rows[pressed_row]) begin
+                            nextstate = col2_hold;
+                       end
+                       else nextstate = col2;
+            col3_hold: if (stable_rows[pressed_row]) begin
+                            nextstate = col3_hold;
+                       end
+                       else nextstate = col3;
             default: nextstate = col0;
         endcase
-    end
-
-    // Pressed Col logic
-    always_ff @(posedge clk) begin
-        if (reset == 0) begin
-            pressed_col <= '0;
-        end 
-        else begin
-            if (state != debounce && nextstate == debounce) begin
-                case (state)
-                    col0_check: pressed_col <= 2'd0;
-                    col1_check: pressed_col <= 2'd1;
-                    col2_check: pressed_col <= 2'd2;
-                    col3_check: pressed_col <= 2'd3;
-                    default: pressed_col <= pressed_col;
-                endcase
-            end
-        end
     end
 
     // Pressed Row logic
@@ -111,39 +90,38 @@ module scanner(
             pressed_row <= 2'd0;
         end 
         else begin
-            if (state != debounce && nextstate == debounce) begin
-                case (stable_rows)
-                    4'b0001: pressed_row <= 2'd0;
-                    4'b0010: pressed_row <= 2'd1;
-                    4'b0100: pressed_row <= 2'd2;
-                    4'b1000: pressed_row <= 2'd3;
-                    default: pressed_row <= pressed_row;
-                endcase
-            end
+            case (stable_rows)
+                4'b0001: pressed_row <= 2'd0;
+                4'b0010: pressed_row <= 2'd1;
+                4'b0100: pressed_row <= 2'd2;
+                4'b1000: pressed_row <= 2'd3;
+                default: pressed_row <= pressed_row;
+            endcase
         end
     end
 
-    // Debounce logic
-    always_ff @(posedge clk)
-        if (reset == 0)
-            counter <= '0;
-        else if (state != debounce && nextstate == debounce)
-            counter <= '0;
-        else if (state == debounce && counter < 50)
-            counter <= counter + '1;
-
     // output logic
     always_comb begin
-        case (state)
-            col0, col0_check:       cols = 4'b0001;
-            col1, col1_check:       cols = 4'b0010;
-            col2, col2_check:       cols = 4'b0100;
-            col3, col3_check:       cols = 4'b1000;
-            debounce, pressed:      cols = 4'b0001 << pressed_col;
-            default:                cols = 4'b0000;
+        // Assign columns
+        case (nextstate)
+            col0, col0_hold:    cols = 4'b0001;
+            col1, col1_hold:    cols = 4'b0010;
+            col2, col2_hold:    cols = 4'b0100;
+            col3, col3_hold:    cols = 4'b1000;
+            default:            cols = 4'b0000;
+        endcase
+
+        case (nextstate)
+            col0, col0_hold:    pressed_col = 2'd0;
+            col1, col1_hold:    pressed_col = 2'd1;
+            col2, col2_hold:    pressed_col = 2'd2;
+            col3, col3_hold:    pressed_col = 2'd3;
+            default:            pressed_col = 2'd0;
+        endcase
+
+        case (nextstate)
+            col0_hold, col1_hold, col2_hold, col3_hold: press = 1;
+            col0, col1, col2, col3:                     press = 0;
         endcase
     end
-
-    assign press = (state == pressed);
-
 endmodule
