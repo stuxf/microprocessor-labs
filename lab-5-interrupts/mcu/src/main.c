c2f2f979d562e31c90b5ef3511178a4973dc898f
user <70670632+stuxf@users.noreply.github.com>
2024-10-28 20:19:17 -0700

    🎨Some setup code - pre polling

---
+++ lab-5-interrupts/mcu/src/main.c
@@ -0,0 +1,40 @@
+/**
+ * Lab 5, Interrupts Lab
+ * Stephen Xu, stxu@g.hmc.edu
+ * Oct 22, 2024
+ */
+
+#include "STM32L432KC.h"
+#include <stm32l432xx.h>
+
+#define QUAD_A PA5
+#define QUAD_B PA4
+#define DELAY_TIM TIM2
+
+int main(void)
+{
+    // Polling approach
+
+    // Enable QUAD_A and QUAD_B as GPIO input
+
+    // Initialize Timer
+
+    while(1) {
+
+        // Motor spins at 600 rpm. 
+        // We want to sample at twice the rate
+        // Makes 120 Pulses per rotation
+        // 120 * 600 / 60 = 1200 Hz
+        // We want to sample at at least 2400 Hz
+        // Delay of 0.41 ms
+        // Unfortunately, the minimum delay we can do is 1 ms
+        // At least with current implementation
+        // We can do more minute delays with different timer config
+        // Unfortunately, I'm lazy.
+        delay_millis(DELAY_TIM, 1);
+    }
+
+    // Interrupt Approach
+}
+
+// Some sort of interrupt handler
