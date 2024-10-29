cf1e2a0da6673662523e64ca41eb123d38d138af
user <70670632+stuxf@users.noreply.github.com>
2024-10-28 20:40:39 -0700

    ✨ Add time and printf support

--- lab-5-interrupts/mcu/src/main.c
+++ lab-5-interrupts/mcu/src/main.c
@@ -7,6 +7,20 @@
 #include "STM32L432KC.h"
 #include <stm32l432xx.h>
 
+// Printf stuff from Kavi
+// Necessary includes for printf to work
+#include <stdio.h>
+#include "stm32l432xx.h"
+
+// Function used by printf to send characters to the laptop
+int _write(int file, char *ptr, int len) {
+  int i = 0;
+  for (i = 0; i < len; i++) {
+    ITM_SendChar((*ptr++));
+  }
+  return len;
+}
+
 #define QUAD_A PA5
 #define QUAD_B PA4
 #define DELAY_TIM TIM2
@@ -23,6 +37,12 @@ int main(void)
     // Initialize Timer
     RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
     initTIM(DELAY_TIM);
+
+    // Some sort of local variables about speed
+    int aRiseTime = 0;
+    int aFallTime = 0;
+    int bRiseTime = 0;
+    int bRiseTime = 0;
 
     while(1) {
 
