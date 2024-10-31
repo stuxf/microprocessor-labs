45ee22562fc6713159c1aeb49a20104a5a7b7fe7
user <70670632+stuxf@users.noreply.github.com>
2024-10-31 16:16:05 -0700

    ✨ Pretty much done yay (interrupt based rev/s)

--- lab-5-interrupts/mcu/src/main.c
+++ lab-5-interrupts/mcu/src/main.c
@@ -27,6 +27,23 @@ int _write(int file, char *ptr, int len)
 #define QUAD_B PA8
 #define DELAY_TIM TIM2
 
+// Some sort of local variables about speed
+volatile uint32_t aRiseTime = 0;
+volatile uint32_t aFallTime = 0;
+volatile uint32_t bRiseTime = 0;
+volatile uint32_t bFallTime = 0;
+
+volatile uint32_t pulses = 0;
+volatile uint32_t pulseTime = 0;
+volatile uint32_t time = 0;
+
+volatile int direction = 1;
+
+volatile int curAState = 0;
+volatile int prevAState = 0;
+volatile int curBState = 0;
+volatile int prevBState = 0;
+
 int main(void)
 {
     // Polling approach
@@ -40,26 +57,15 @@ int main(void)
     RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
     initTIM(DELAY_TIM);
 
-    // Some sort of local variables about speed
-    uint32_t aRiseTime = 0;
-    uint32_t aFallTime = 0;
-    uint32_t bRiseTime = 0;
-    uint32_t bFallTime = 0;
-
-    uint32_t pulses = 0;
-    uint32_t pulseTime = 0;
-    uint32_t time = 0;
-
     float revsPerSec;
 
     // Local variables about the current state
-    int volatile curAState = digitalRead(QUAD_A);
-    int volatile prevAState = curAState;
-    int volatile curBState = digitalRead(QUAD_B);
-    int volatile prevBState = curBState;
-    int direction = 1;
+    curAState = digitalRead(QUAD_A);
+    prevAState = curAState;
+    curBState = digitalRead(QUAD_B);
+    prevBState = curBState;
 
-    while (1)
+    while (0)
     {
         // Motor spins at 600 rpm.
         // We want to sample at twice the rate
@@ -70,8 +76,8 @@ int main(void)
         // So we delay for 200 microseconds
         // TIM2 is a CHONKY 32 bit timer so we chilling asf
         // Can keep this going for an hour haha
-        delay_micros(TIM2, 200);
-        time += 200;
+        delay_micros(TIM2, 100);
+        time += 100;
 
         curAState = digitalRead(QUAD_A);
         curBState = digitalRead(QUAD_B);
@@ -80,21 +86,17 @@ int main(void)
         if ((prevAState == 0) && (curAState == 1))
         {
             aRiseTime = time;
-            if (curBState)
-            {
-                direction = 1;
-                pulseTime += aRiseTime - bRiseTime;
-            }
-            else
-            {
-                direction = -1;
-                pulseTime += bRiseTime - aRiseTime;
+            if(curBState) {
+                direction += 1;
             }
             pulses++;
         }
         // Rising Edge B
         if ((prevBState == 0) && (curBState == 1))
         {
+            if (curAState) {
+                direction -= 1;
+            }
             bRiseTime = time;
             pulses++;
         }
@@ -113,6 +115,14 @@ int main(void)
         if (time % 100000 == 0 && time != 0)
         {
             // Running average of pulses
+            if (direction > 0)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = -1;
+            }
             revsPerSec = direction * (pulses * 1.0) / (100000 * 1.0) * (1.0 / 120.0) * (1.0 / 4.0) * 1000000;
             printf("Rev/s: %f\n", revsPerSec);
             printf("Debug info: %d, %d, %d \n", aRiseTime, bRiseTime, time);
@@ -120,6 +130,7 @@ int main(void)
             printf("Debug info 3: %d, %d, %d \n", direction, pulses, pulseTime);
             pulses = 0;
             pulseTime = 0;
+            direction = 0;
         }
         // check state changes
         prevBState = curBState;
@@ -152,20 +163,79 @@ int main(void)
 
     // Turn on IRQ interrupt, but for PA5-9 ... (which PA5, PA8 fall under)
     NVIC->ISER[0] |= 1 << EXTI9_5_IRQn;
+
+    while (1)
+    {
+        delay_micros(TIM2, 500000);
+        if (direction > 0)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+        }
+        revsPerSec = direction * (pulses * 1.0) / (400000 * 1.0) * (1.0 / 120.0) * (1.0 / 4.0) * 1000000;
+        printf("Rev/s: %f\n", revsPerSec);
+        //printf("Debug info: %d, %d, %d \n", aRiseTime, bRiseTime, time);
+        //printf("Debug info 2: %d, %d \n", prevAState, curAState);
+        //printf("Debug info 3: %d, %d, %d \n", direction, pulses, pulseTime);
+        pulses = 0;
+        pulseTime = 0;
+        direction = 0;
+    }
 }
 
 // Some sort of interrupt handler
 void EXTI9_5_IRQHandler(void)
 {
+    curAState = digitalRead(QUAD_A);
+    curBState = digitalRead(QUAD_B);
+    time = TIM2->CNT;
     // Check which pin (s) activated interrupt
     if (EXTI->PR1 & (1 << gpioPinOffset(QUAD_A)))
     {
+        // printf("A interrupt!\n");
+        // Rising Edge A
+        if ((prevAState == 0) && (curAState == 1))
+        {
+            aRiseTime = time;
+            if(curBState) {
+                direction += 1;
+            }
+            pulses++;
+        }
+        // Falling Edge A
+        else if ((prevAState == 1) && (curAState == 0))
+        {
+            aFallTime = time;
+            pulses++;
+        }
         // If so, clear the interrupt (NB: Write 1 to reset.)
         EXTI->PR1 |= (1 << gpioPinOffset(QUAD_A));
     }
     if (EXTI->PR1 & (1 << gpioPinOffset(QUAD_B)))
     {
+        // printf("B interrupt!\n");
+        // Rising Edge
+        if ((prevBState == 0) && (curBState == 1))
+        {
+            bRiseTime = time;
+            if (curAState) {
+                direction -= 1;
+            }
+            pulses++;
+        }
+        // Falling Edge
+        else if ((prevBState == 1) && (curBState == 0))
+        {
+            bFallTime = time;
+            pulses++;
+        }
         // If so, clear the interrupt (NB: Write 1 to reset.)
         EXTI->PR1 |= (1 << gpioPinOffset(QUAD_B));
     }
+    // check state changes
+    prevBState = curBState;
+    prevAState = curAState;
 }
