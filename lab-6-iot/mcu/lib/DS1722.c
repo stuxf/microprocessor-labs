4f5f12a8ce4fbabe3abd2d2616f318bc9e376342
user <70670632+stuxf@users.noreply.github.com>
2024-11-03 01:05:10 -0700

    ✨ DS1722 Code!

--- lab-6-iot/mcu/lib/DS1722.c
+++ lab-6-iot/mcu/lib/DS1722.c
@@ -5,24 +5,35 @@
 // Implementation code for interactions with DS1722 Temp Sensor
 
 #include "DS1722.h"
+#include "STM32L432KC_SPI.h"
 
 void initSensor(int resolution)
 {
     // OPERATION-Programming Section
     // See Figure 2
     // Write 0x80 to indicate config
+    spiSendReceive(0x80);
     // Set MSB to 111
     // Don't want one shot
     // Set lsb (shutdown bit) to 1 for continuous (0 for shutdown if low power)
+    char config = 0b11100001;
+    config |= resolution << 1;
+    spiSendReceive(config);
 }
 
 double readTemp()
 {
     // Chip enable
+    digitalWrite(CS_PIN, PIO_HIGH);
     // Read MSB (send 0x2)
+    spiSendReceive(0x2);
     // Send 0x0 to get
+    char msb = spiSendReceive(0x0);
     // Read LSB (send 0x1)
+    spiSendReceive(0x1);
     // Send 0x0 to get
+    char lsb = spiSendReceive(0x1);
     // Set to low
     // Chip disable
+    digitalWrite(CS_PIN, PIO_LOW);
 }
--- lab-6-iot/mcu/lib/DS1722.h
+++ lab-6-iot/mcu/lib/DS1722.h
@@ -7,6 +7,17 @@
 #ifndef DS1722_H
 #define DS1722_H
 
+#include "STM32L432KC.h"
+#include "STM32L432KC_SPI.h"
+
+#define CS_PIN PA8
+
+#define EIGHT_BIT 0b000
+#define NINE_BIT 0b001
+#define TEN_BIT 0b010
+#define ELEVEN_BIT 0b011
+#define TWELVE_BIT 0b100
+
 void initSensor(int resolution);
 
 double readTemp();
